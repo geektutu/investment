8903569a5876dda275714c24f4ed9fa107de7175
geektutu <geektutu@users.noreply.github.com>
2026-07-27 15:59:56 +0000

    deploy: 1af2064f70d7128d2a2e77672392b408e6b3146c

---
+++ _nuxt/K2Ye4sOQ.js
@@ -0,0 +1,1 @@
+import{A as t}from"./DQz23Lqt.js";import{s as r,o as a}from"./CWPh7kd1.js";const p={__name:"stock-atr",setup(o){return(s,c)=>(a(),r(t,{csvPath:"stock_atr.csv"}))}};export{p as default};
